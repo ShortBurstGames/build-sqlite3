99bc31efc377532395e30b854ffaba58fa0ca192
Micah Stetson <micah@shortburstgames.com>
2025-06-14 19:43:28 -0700

    Alter build strategy a bit

--- build.sh
+++ build.sh
@@ -6,20 +6,15 @@ rm -f *.o *.obj *.a *.lib
 mkdir -p lib/$1
 
 case $1 in
-	arm64-osx|x86_64-osx)
+	*-osx)
 		# Build both architectures for both architectures.
 		set -x
 		clang -arch arm64 -arch x86_64 -c $DEFINES sqlite3.c lsqlite3.c
 		ar rcs lib/$1/libsqlite3.a sqlite3.o lsqlite3.o
 	;;
-	x86_64-linux)
+	*-linux)
 		set -x
-		clang -arch x86_64 -c $DEFINES sqlite3.c lsqlite3.c
-		ar rcs lib/$1/libsqlite3.a sqlite3.o lsqlite3.o
-	;;
-	arm64-linux)
-		set -x
-		clang -arch arm64 -c $DEFINES sqlite3.c lsqlite3.c
+		clang -c $DEFINES sqlite3.c lsqlite3.c
 		ar rcs lib/$1/libsqlite3.a sqlite3.o lsqlite3.o
 	;;
 	x86_64-win32)
